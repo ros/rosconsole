af8c4ba4ac5d3052374e08ed422d5df4cdece445
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-05-21 08:48:00 -0700

    remove all packages except rosconsole

--- tools/roslaunch/env-hooks/10.roslaunch.bat
+++
@@ -1,5 +0,0 @@
-REM roslaunch/env-hooks/10.roslaunch.bat
-
-if "%ROS_MASTER_URI%" == "" (
-  set ROS_MASTER_URI=http://localhost:11311
-)
--- tools/roslaunch/env-hooks/10.roslaunch.sh
+++
@@ -1,5 +0,0 @@
-# roslaunch/env-hooks/10.roslaunch.sh
-
-if [ ! "$ROS_MASTER_URI" ]; then
-  export ROS_MASTER_URI=http://localhost:11311
-fi
